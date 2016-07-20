00c8bde2b56a796be69e5c7a02de6d347ceddf64
lampasaurus <gremlinpower@gmail.com>
2016-07-20 14:58:59 -0700

    changed test values

--- tester.js
+++ tester.js
@@ -4,12 +4,12 @@ board = back.createGame('ai', 6);
 console.log(board.readBoard());
 
 back.connect(1, 2);
-back.getMove(board, 0, 0, 0, function n(){});
-back.getMove(board, 1, 2, 0, function n(){});
+back.getMove(board, 0, 3, 0, function n(){});
+back.getMove(board, 3, 2, 0, function n(){});
 back.getMove(board, 2, 5, 1, function n(){});
-back.getMove(board, 3, 2, 2, function n(){});
+back.getMove(board, 3, 3, 2, function n(){});
 back.getMove(board, 2, 2, 1, function n(){});
-back.getMove(board, 2, 0, 2, function n(){});
+back.getMove(board, 2, 5, 2, function n(){});
 
 console.log(board.readBoard());
 
